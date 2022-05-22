96d136d30803db50f0d0d903db8a841790c46e1d
h2j0 <heejeong0527@gmail.com>
2022-05-22 21:57:32 +0900

    video card commit

--- jungfit-pjt-front/src/components/videos/VideoList.vue
+++ jungfit-pjt-front/src/components/videos/VideoList.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
-  
+  <br>
   <div class="text-center">
-    <v-btn @click="search" v-model="keyword"
-      value="0"
+    <v-btn @click="search('전신 운동')"
       class="ma-2"
       outlined
       color="indigo"
@@ -11,8 +10,7 @@
     전신
     </v-btn>
 
-    <v-btn @click="search" v-model="keyword"
-      value="1"
+    <v-btn @click="search('상체 운동')"
       class="ma-2"
       outlined
       color="indigo"
@@ -20,8 +18,7 @@
     상체
     </v-btn>
 
-    <v-btn @click="search" v-model="keyword"
-      value="2"
+    <v-btn @click="search('하체 운동')" 
       class="ma-2"
       outlined
       color="indigo"
@@ -29,8 +26,7 @@
     하체
     </v-btn>
 
-    <v-btn @click="search" v-model="keyword"
-      value="3"
+    <v-btn @click="search('복부 운동')"
       class="ma-2"
       outlined
       color="indigo"
@@ -38,27 +34,93 @@
     복부
     </v-btn>
     </div>
+
+    <v-container 
+     :key="start"   
+      class="grey lighten-5 mb-6"
+    >
+      <v-row
+        :align="start"
+        no-gutters
+        style="height: 150px;"
+      >
+        <v-col
+          v-for="n in 3"
+          :key="n"
+        >
+          
+        <v-card 
+    class="mx-auto"
+    max-width="360"
+  >
+  
+ <iframe
+            width="360"
+            height="220"
+            :src="'https://www.youtube.com/embed/'+'gMaB-fG4u4g'"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+
+
+
+    <v-card-subtitle class="pb-0">
+      Number 10
+    </v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>Whitehaven Beach</div>
+
+      <div>Whitsunday Island, Whitsunday Islands</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn>
+        리뷰
+      </v-btn>
+
+       <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+      
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+    </v-card-actions>
+  </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+
+
+
     </div>
 </template>
 
 <script>
+import {mapState} from "vuex";
 export default {
   name:"videoList",
   data(){
     return{
-      keywordlist:["전신","상체","하체","복부"],
-      keyword:0,
+      defaultKey:"전신 운동"
     }
   },
+  computed:{
+    ...mapState(["videos"])
+  },
    created(){
-    this.$store.dispatch("getPartList", this.keywordlist[0])
+    this.$store.dispatch("getPartList", this.defaultKey)
   },
   methods:{
-    search(){
-      const payload = {
-        keyword: this.keyword
-      }
-       this.$store.dispatch("getPartList", this.keywordlist[payload]) 
+    search(value){
+       this.$store.dispatch("getPartList", value) 
     }
   }
 
