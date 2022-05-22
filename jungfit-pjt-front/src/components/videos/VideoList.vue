7d7f7e625f3adfe68cce56e93bc1f23c1d1047f7
h2j0 <heejeong0527@gmail.com>
2022-05-22 18:07:24 +0900

    nav bar commit

---
+++ jungfit-pjt-front/src/components/videos/VideoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  
+    <div >
+      <h2>비디오리스트</h2>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name:"videoList"
+}
+</script>
+
+<style>
+
+</style>
