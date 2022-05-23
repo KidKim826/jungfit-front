e5fffff6c285452de6ed69848bcc152a727dfb03
KidKim826 <moopama4@gmail.com>
2022-05-23 09:50:37 +0900

    video commit

--- jungfit-pjt-front/src/store/index.js
+++ jungfit-pjt-front/src/store/index.js
@@ -5,6 +5,8 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const REST_API = `http://localhost:9999/jungfit`
+
 export default new Vuex.Store({
     // plugins: [createPersistateState()],
     state: {
@@ -14,6 +16,11 @@ export default new Vuex.Store({
         c: [],
         d: [],
         selectedVideo: [],
+        topVideos:[],
+        t1: [],
+        t2: [],
+        t3: [],
+        t4: [],
     },
     getters: {},
     mutations: {
@@ -23,6 +30,17 @@ export default new Vuex.Store({
             state.b = [value[3], value[4], value[5]]
             state.c = [value[6], value[7], value[8]]
             state.d = [value[9], value[10], value[11]]
+        },
+        GET_TOP_LIST(state, value) {
+            state.topVideos = value
+            state.t1 = [value[0], value[1], value[2]]
+            state.t2 = [value[3], value[4], value[5]]
+            state.t3 = [value[6], value[7], value[8]]
+            state.t4 = [value[9], value[10], value[11]]
+            console.log(state.t1)
+            console.log(state.t2)
+            console.log(state.t3)
+            console.log(state.t4)
         },
         GET_VREVIEW_LIST(state, value) {
             // console.log(value)
@@ -38,7 +56,25 @@ export default new Vuex.Store({
 
     },
     actions: {
-        getPartList({ commit }, value) {
+        getTopList({commit}, value) {
+            let params = null
+
+            if(value) { //들어오는 payload가 있다면
+              params = value //params는 payload로
+            }
+            const API_URL=`${REST_API}/review/video`
+            axios({
+              url: API_URL,
+              method:'GET',
+              params, //그걸 같이 넘겨줘
+            }).then((res) => {
+              console.log(res)
+              commit('GET_TOP_LIST', res.data)
+            }).catch((err) => {
+              console.log(err)
+            })    
+        },
+        getYoutubeList({ commit }, value) {
             const YOUTUBE_KEY = process.env.VUE_APP_YOUTUBE_API_KEY;
             const API_URL = `https://www.googleapis.com/youtube/v3/search`
             const params = {
