9038a9b2541de2b4de0f2467720aa8a3f09b7236
KidKim826 <moopama4@gmail.com>
2022-05-23 14:19:52 +0900

    monday

--- jungfit-pjt-front/src/router/index.js
+++ jungfit-pjt-front/src/router/index.js
@@ -6,29 +6,34 @@ import VideoView from '@/views/VideoView.vue'
 import VideoList from '@/components/videos/VideoList.vue'
 import VReviewList from '@/components/videos/VReviewList.vue'
 // import VReviewDetail from '@/components/videos/VReviewDetail.vue'
-
+import VReviewView from '@/views/VReviewView.vue'
 Vue.use(VueRouter)
 
 const routes = [{
-        path: '/',
-        name: 'home',
-        component: HomeView
-    },
-    {
-        path: '/video',
-        name: 'video',
-        component: VideoView,
-        children: [{
-            path: "",
-            name: "videoList",
-            component: VideoList
-        }, {
-            path: "review/:id",
+    path: '/',
+    name: 'home',
+    component: HomeView
+},
+{
+    path: '/video',
+    name: 'video',
+    component: VideoView,
+    children: [{
+        path: "",
+        name: "videoList",
+        component: VideoList
+    }],
+}, {
+    path: '/review',
+    name: 'review',
+    component: VReviewView,
+    children: [{
+            path: "video/:id",
             name: "VReviewList",
             component: VReviewList
-        }]
-
-    }
+        },
+    ]
+}
 ]
 
 const router = new VueRouter({
