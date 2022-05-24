d5bccbda285e0c28a73f13379a9926033ecb3b45
h2j0 <heejeong0527@gmail.com>
2022-05-24 11:38:59 +0900

    setting commit

--- jungfit-pjt-front/src/main.js
+++ jungfit-pjt-front/src/main.js
@@ -5,25 +5,15 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-<<<<<<< HEAD
-=======
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
->>>>>>> 2e34acd5e80913319d464c53a8de9d441495837f
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
-<<<<<<< HEAD
     // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
-=======
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
 
->>>>>>> 2e34acd5e80913319d464c53a8de9d441495837f
 Vue.config.productionTip = false
 
 new Vue({
