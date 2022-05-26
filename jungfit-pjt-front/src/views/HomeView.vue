b396d19b7de3eb16b4f744fee61f59c3007cb475
KidKim826 <moopama4@gmail.com>
2022-05-26 09:43:42 +0900

    merge commit

--- jungfit-pjt-front/src/views/HomeView.vue
+++ jungfit-pjt-front/src/views/HomeView.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
     <hello-jungfit />
-    <v-row no-gutters style="margin-top: 20px;">
-      <v-col cols="12" lg="4">
-        
-      </v-col>
-      <v-col cols="12" lg="4">
-        
-
-      </v-col>
-    </v-row>
   </div>
 </template>
 
